dd9fd1a7bed1ab769ddbdcfeefa1c4567853682c
Kok-Wei Bong <bongkokwei@gmail.com>
2025-05-10 00:19:56 +1000

    minor edit

--- src/rsoft_cad/geometry/__init__.py
+++ src/rsoft_cad/geometry/__init__.py
@@ -3,4 +3,8 @@ This package calculates the geometry of a taper
 """
 
 from .tapers import calculate_taper_properties
-from .custom_taper import sigmoid_taper_ratio, plot_combined_taper
+from .custom_taper import (
+    sigmoid_taper_ratio,
+    plot_combined_taper,
+    model_photonic_lantern_taper,
+)
